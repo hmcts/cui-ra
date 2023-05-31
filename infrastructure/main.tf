a315fe79b54bb930fea957887cadcb74f59b8c19
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-05-31 12:44:35 +0100

    removed redis from terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -46,26 +46,22 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   resource_group_name = "core-infra-${var.env}"
 }
 
-module "cui-ra-redis-cache" {
-  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product     = "${var.product}-${var.component}-redis-cache"
-  location = var.location
-  env      = var.env
-  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
-  common_tags  = var.common_tags
-}
+//module "cui-ra-redis-cache" {
+//  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+//  product     = "${var.product}-${var.component}-redis-cache"
+//  location = var.location
+//  env      = var.env
+//  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
+//  common_tags  = var.common_tags
+//}
 
-data "azurerm_key_vault" "key_vault" {
-  name = local.vaultName
-  resource_group_name = local.vaultName
-}
 
 ////////////////////////////////
 // Populate Vault with redis info
 ////////////////////////////////
 
-resource "azurerm_key_vault_secret" "redis_access_key" {
-  name         = "${var.component}-redis-access-key"
-  value        = module.cui-ra-redis-cache.access_key
-  key_vault_id = data.azurerm_key_vault.key_vault.id
-}
+//resource "azurerm_key_vault_secret" "redis_access_key" {
+//  name         = "${var.component}-redis-access-key"
+//  value        = module.cui-ra-redis-cache.access_key
+//  key_vault_id = data.azurerm_key_vault.key_vault.id
+//}
