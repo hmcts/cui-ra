816bec585412e789f5ab119ef0a1c520d8ec861b
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-05-31 11:36:16 +0100

    created the required terraform files

---
+++ infrastructure/main.tf
@@ -0,0 +1,69 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  app_full_name = "${var.product}-${var.component}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-shared-${var.env}"
+  location = var.location
+  tags = var.common_tags
+}
+
+module "key-vault" {
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  product             = var.product
+  env                 = var.env
+  tenant_id           = var.tenant_id
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
+  product_group_name  = "dcd_ccd"
+  common_tags         = var.common_tags
+  create_managed_identity    = true
+}
+
+resource "azurerm_key_vault_secret" "AZURE_APPINSGHTS_KEY" {
+  name         = "AppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.appinsights.instrumentation_key
+  key_vault_id = module.key-vault.key_vault_id
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.env}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+  tags                = var.common_tags
+}
+
+data "azurerm_subnet" "core_infra_redis_subnet" {
+  name                 = "core-infra-subnet-1-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+  resource_group_name = "core-infra-${var.env}"
+}
+
+module "cui-ra-redis-cache" {
+  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product     = "${var.product}-${var.component}-redis-cache"
+  location = var.location
+  env      = var.env
+  subnetid = data.azurerm_subnet.core_infra_redis_subnet.id
+  common_tags  = var.common_tags
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name = local.vaultName
+  resource_group_name = local.vaultName
+}
+
+////////////////////////////////
+// Populate Vault with redis info
+////////////////////////////////
+
+resource "azurerm_key_vault_secret" "redis_access_key" {
+  name         = "${var.component}-redis-access-key"
+  value        = module.cui-ra-redis-cache.access_key
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
