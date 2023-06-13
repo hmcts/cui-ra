c27006a6d5cfc512782d1f29f1e8b87110bc8aa2
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-06-13 11:53:30 +0100

    removed commented code

--- src/main/controllers/home.controller.ts
+++ src/main/controllers/home.controller.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from 'express';
-//import { S2S } from './../services';
 
 export class HomeController {
   public get(req: Request, res: Response): void {
-    //const s2s = new S2S();
-    //const token = s2s.getToken();
-    //res.send(token);
-    //return;
     res.render('home');
   }
 }
