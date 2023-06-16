21c55173eab8a9962a0580f15e08f0c0f9b37f7c
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-06-16 14:46:51 +0100

    added more unit tests

--- src/test/unit/tests/services/s2s.test.ts
+++ src/test/unit/tests/services/s2s.test.ts
@@ -13,15 +13,36 @@ describe('s2s service class', () => {
   };
 
   const service = new S2S('randomstring', 'servicename', axios, mockedLogger);
-  (axios.post as jest.Mock).mockImplementation(() => Promise.resolve(response));
 
-  test('get service token', async () => {
+  test('Get Service Token', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
+    (axios.post as jest.Mock).mockImplementation(() => Promise.resolve(response));
     expect(await service.getToken()).toEqual(expectedToken);
   });
 
-  test('validate service token', async () => {
+  test('Get Service Token but throw an error', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
+    (axios.post as jest.Mock).mockRejectedValueOnce({ error: 'Internal Server Error' });
+    try {
+      await service.getToken();
+    } catch (error) {
+      expect(error).toEqual({ error: 'Internal Server Error' });
+    }
+  });
+
+  test('Validate Service Token successfully', async () => {
+    // eslint-disable-line @typescript-eslint/no-empty-function
+    (axios.post as jest.Mock).mockImplementation(() => Promise.resolve(response));
     expect(await service.validateToken(expectedToken)).toEqual(expectedToken);
+  });
+
+  test('Validate Service Token but throw an error', async () => {
+    // eslint-disable-line @typescript-eslint/no-empty-function
+    (axios.post as jest.Mock).mockRejectedValueOnce({ error: 'Internal Server Error' });
+    try {
+      await service.validateToken(expectedToken);
+    } catch (error) {
+      expect(error).toEqual({ error: 'Internal Server Error' });
+    }
   });
 });
