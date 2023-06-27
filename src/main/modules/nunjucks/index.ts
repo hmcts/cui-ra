dc008b789fc48a3f47b1762f8c0b4318f3d4ae24
Ben James <ben.james@hmcts.net>
2023-06-27 16:55:38 +0100

    Amended nunjucks to get correct value from resource files, including service and language fallbacks

--- src/main/modules/nunjucks/index.ts
+++ src/main/modules/nunjucks/index.ts
@@ -3,8 +3,6 @@ import * as path from 'path';
 import * as express from 'express';
 import * as nunjucks from 'nunjucks';
 
-const i18n = require('i18n');
-
 export class Nunjucks {
   constructor(public developmentMode: boolean) {
     this.developmentMode = developmentMode;
@@ -12,17 +10,38 @@ export class Nunjucks {
 
   enableFor(app: express.Express): void {
     app.set('view engine', 'njk');
-    const env = nunjucks.configure(path.join(__dirname, '..', '..', 'views'), {
+    nunjucks.configure(path.join(__dirname, '..', '..', 'views'), {
       autoescape: true,
       watch: this.developmentMode,
       express: app,
     });
 
-    env.addGlobal('__', i18n.__);
-    env.addFilter('t', i18n.__);
-
     app.use((req, res, next) => {
       res.locals.pagePath = req.path;
+      res.locals._t = (key: string) => {
+        const serviceId = 'PFL'; // The service ID will need to be pulled from the cache
+
+        if (!serviceId) {
+          return res.__(key);
+        }
+
+        const serviceKey = `${serviceId}.${key}`;
+        const fallback = res.__(key);
+        let result = res.__(`${serviceKey}:${fallback}`);
+
+        if (result !== (serviceKey || key)) {
+          return result;
+        }
+
+        result = res.__(serviceKey);
+
+        if (result !== serviceKey) {
+          return result;
+        }
+
+        return res.__(key);
+      };
+
       next();
     });
   }
