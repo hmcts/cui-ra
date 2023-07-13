4c793520c14693b8e4a6915076f0cb190d18b6c2
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-07-13 08:27:31 +0100

    removed commented code

--- src/main/interfaces/dataManager.ts
+++ src/main/interfaces/dataManager.ts
@@ -15,31 +15,11 @@ export interface DataManagerDataObject extends PayloadCollectionItem {
 export interface DataManager<T> {
   data: T[];
   modified: boolean;
-
   //Set the data to the manager
   set(data: T[]): void;
-
   //Save partial data into the manager
   save(data: Partial<T>[]): void;
-
   get(id: string): T | null;
-
   find(key: string, value: string): T[];
-
   getDateTime(): string;
-
-  //getChildren(id:string): T[];
-
-  //enable(id:string): void;
-
-  //disable(id:string): void;
-
-  //init(flags: ReferenceDataFlagType, includeParent: boolean): void;
-  //getFlag(flagCode: string, parentCode: string | null): T | null;
-  //getChildren(flagCode: string): T[];
-  //enable(flagCode: string, parentCode: string | null): void;
-  //disable(flagCode: string, parentCode: string | null, disableDependants: boolean): void;
-  //find(key: string, value: string): T[];
-  //getNext(currentFlagCode: string): T | null;
-  //getPrevious(currentFlagCode: string): T | null;
 }
