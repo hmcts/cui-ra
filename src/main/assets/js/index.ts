e5daab0db372bb20f6530d361847a2ac8dbd70d5
Ben James <ben.james@hmcts.net>
2023-07-27 18:43:14 +0100

    Added additional components to dynamic form including new accessible autocomplete

--- src/main/assets/js/index.ts
+++ src/main/assets/js/index.ts
@@ -1,4 +1,11 @@
 import '../scss/main.scss';
 import { initAll } from 'govuk-frontend';
+import accessibleAutocomplete from 'accessible-autocomplete';
 
 initAll();
+
+accessibleAutocomplete.enhanceSelectElement({
+    defaultValue: '',
+    selectElement: document.querySelector('#country'),
+    confirmOnBlur: false,
+});
--- src/main/controllers/formController.ts
+++ src/main/controllers/formController.ts
@@ -4,6 +4,27 @@ import { Request, Response } from 'express';
 @autobind
 export class FormController {
   public async get(req: Request, res: Response): Promise<void> {
+    res.locals.opts = 
+    [
+      {
+        value: "",
+        text: "Select a country"
+      },
+      {
+        value: "France",
+        text: "France"
+      },
+      {
+        value: "Wales",
+        text: "Wales",
+        selected: true
+      },
+      {
+        value: "England",
+        text: "England"
+      }
+    ]
+
     res.render('dynamic-form');
   }
 }
