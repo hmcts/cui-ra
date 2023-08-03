9bcc7d757813cb133cc533ef74968bfe1ecbc418
Ben James <ben.james@hmcts.net>
2023-08-03 09:49:07 +0100

    Added variables to render dummy accessible autocomplete

--- src/main/controllers/formController.ts
+++ src/main/controllers/formController.ts
@@ -4,26 +4,25 @@ import { Request, Response } from 'express';
 @autobind
 export class FormController {
   public async get(req: Request, res: Response): Promise<void> {
-    res.locals.opts = 
-    [
+    res.locals.opts = [
       {
-        value: "",
-        text: "Select a country"
+        value: '',
+        text: 'Select a country',
       },
       {
-        value: "France",
-        text: "France"
+        value: 'France',
+        text: 'France',
       },
       {
-        value: "Wales",
-        text: "Wales",
-        selected: true
+        value: 'Wales',
+        text: 'Wales',
+        selected: true,
       },
       {
-        value: "England",
-        text: "England"
-      }
-    ]
+        value: 'England',
+        text: 'England',
+      },
+    ];
 
     res.render('dynamic-form');
   }
