4898cd59d5c5af41562925afe545ca648725b0b8
Ben James <ben.james@hmcts.net>
2023-08-04 10:30:55 +0100

    Added route for new review page

--- src/main/controllers/homeController.ts
+++ src/main/controllers/homeController.ts
@@ -6,4 +6,16 @@ export class HomeController {
   public async get(req: Request, res: Response): Promise<void> {
     res.render('home');
   }
+
+  public async overview(req: Request, res: Response): Promise<void> {
+    res.render('overview');
+  }
+
+  public async intro(req: Request, res: Response): Promise<void> {
+    res.render('intro');
+  }
+
+  public async review(req: Request, res: Response): Promise<void> {
+    res.render('review');
+  }
 }
