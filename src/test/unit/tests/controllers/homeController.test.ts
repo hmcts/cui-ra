aba34284f062224dc05f927fb24217dc688c6152
Ben James <ben.james@hmcts.net>
2023-08-09 11:33:22 +0100

    updated overview test to set session data as is now expected

--- src/test/unit/tests/controllers/homeController.test.ts
+++ src/test/unit/tests/controllers/homeController.test.ts
@@ -1,38 +1,76 @@
+import fs from 'fs';
+import { Request, Response } from 'express';
 import { HomeController } from '../../../../main/controllers';
-import { mockRequest, mockResponse } from '../../mocks';
+import { Session, SessionData } from 'express-session';
+import { PayloadCollectionItem } from '../../../../main/interfaces';
+import { ExistingFlagsManager } from '../../../../main/managers';
+
+const host = 'www.test.com';
+const protocol = 'https';
 
 /* eslint-disable jest/expect-expect */
 describe('Home Controller', () => {
-  const controller = new HomeController();
+  let homeController: HomeController;
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+
+  beforeEach(() => {
+    // Initialize the mock objects and dependencies
+    mockRequest = {
+      body: {},
+      params: {},
+      session: { partyName: '', existingManager: {} } as unknown as Session & Partial<SessionData>,
+      protocol: protocol,
+      headers: {
+        host: host,
+      },
+    };
+    mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+      redirect: jest.fn().mockReturnThis(),
+      render: jest.fn().mockReturnThis(),
+    };
+    homeController = new HomeController();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Should render home page', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
-    const req = mockRequest(null);
-    const res = mockResponse();
-    controller.get(req, res);
-    expect(res.render).toBeCalledWith('home');
+    homeController.get(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.render).toBeCalledWith('home');
   });
 
   test('Should render overview page', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
-    const req = mockRequest(null);
-    const res = mockResponse();
-    controller.overview(req, res);
-    expect(res.render).toBeCalledWith('overview');
+    const existingJson: PayloadCollectionItem[] = JSON.parse(
+      fs.readFileSync(__dirname + '/../../../../main/demo/data/demo-payload.json', 'utf-8')
+    );
+    const dataManagerExisting: ExistingFlagsManager = new ExistingFlagsManager();
+    dataManagerExisting.set(existingJson);
+
+    // Need to set stuff on an actual session here and not the request??
+    mockRequest.session = {
+      partyName: 'demo',
+      existingManager: dataManagerExisting,
+    } as unknown as Session & Partial<SessionData>;
+
+    homeController.overview(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.render).toBeCalledWith('overview', expect.any(Object));
   });
 
   test('Should render intro page', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
-    const req = mockRequest(null);
-    const res = mockResponse();
-    controller.intro(req, res);
-    expect(res.render).toBeCalledWith('intro');
+    homeController.intro(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.render).toBeCalledWith('intro');
   });
 
   test('Should render review page', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
-    const req = mockRequest(null);
-    const res = mockResponse();
-    controller.review(req, res);
-    expect(res.render).toBeCalledWith('review', expect.any(Object));
+    homeController.review(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.render).toBeCalledWith('review', expect.any(Object));
   });
 });
