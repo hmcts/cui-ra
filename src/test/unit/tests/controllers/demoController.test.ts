141e164ea4957d297778c49a563fdc7aad54bde3
Ben James <ben.james@hmcts.net>
2023-08-09 11:32:48 +0100

    added tests for new demo controller methods

--- src/test/unit/tests/controllers/demoController.test.ts
+++ src/test/unit/tests/controllers/demoController.test.ts
@@ -1,14 +1,73 @@
+import fs from 'fs';
+import { Request, Response } from 'express';
 import { DemoController } from '../../../../main/controllers';
-import { mockRequest, mockResponse } from '../../mocks';
+import { Session, SessionData } from 'express-session';
+import { PayloadCollectionItem } from '../../../../main/interfaces';
+import { ExistingFlagsManager } from '../../../../main/managers';
+
+const host = 'www.test.com';
+const protocol = 'https';
 
 /* eslint-disable jest/expect-expect */
 describe('Demo Controller', () => {
-  const controller = new DemoController();
+  let demoController: DemoController;
+  let mockRequest: Partial<Request>;
+  let mockResponse: Partial<Response>;
+
+  beforeEach(() => {
+    // Initialize the mock objects and dependencies
+    mockRequest = {
+      body: { action: 'demo' },
+      params: {},
+      session: { partyName: '', existingManager: {} } as unknown as Session & Partial<SessionData>,
+      protocol: protocol,
+      headers: {
+        host: host,
+      },
+    };
+    mockResponse = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+      redirect: jest.fn().mockReturnThis(),
+      render: jest.fn().mockReturnThis(),
+    };
+    demoController = new DemoController();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test('Should render demo page', async () => {
     // eslint-disable-line @typescript-eslint/no-empty-function
-    const req = mockRequest(null);
-    const res = mockResponse();
-    controller.get(req, res);
-    expect(res.render).toBeCalledWith('demo');
+    await demoController.get(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.render).toBeCalledWith('demo');
+  });
+
+  test('Should render intro page', async () => {
+    // eslint-disable-line @typescript-eslint/no-empty-function
+    mockRequest.body = { action: 'new' };
+
+    demoController.post(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.redirect).toBeCalledWith('home/intro');
+  });
+
+  test('Should render overview page', async () => {
+    // eslint-disable-line @typescript-eslint/no-empty-function
+    const existingJson: PayloadCollectionItem[] = JSON.parse(
+      fs.readFileSync(__dirname + '/../../../../main/demo/data/demo-payload.json', 'utf-8')
+    );
+    const dataManagerExisting: ExistingFlagsManager = new ExistingFlagsManager();
+    dataManagerExisting.set(existingJson);
+
+    mockRequest.body = { action: 'existing' };
+
+    mockRequest.session = {
+      partyName: 'demo',
+      existingManager: dataManagerExisting,
+    } as unknown as Session & Partial<SessionData>;
+
+    demoController.post(mockRequest as Request, mockResponse as Response);
+    expect(mockResponse.redirect).toBeCalledWith('home/overview');
   });
 });
