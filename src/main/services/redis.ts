7719f348b1c83b89e6d845b447d4ad717583abdb
Ben James <ben.james@hmcts.net>
2023-08-17 16:23:41 +0100

    Removed legacy option from redis constructor

--- src/main/services/redis.ts
+++ src/main/services/redis.ts
@@ -13,8 +13,7 @@ export class RedisClient implements RedisClientInterface {
   constructor(private logger: Logger, private host: string, private port: string, private key: string) {
     this.url = `redis://:**REDACTED**@${this.host}:${this.port}`;
     this.client = createClient({
-      url: `redis://:${this.key}@${this.host}:${this.port}`,
-      legacyMode: true,
+      url: `redis://:${this.key}@${this.host}:${this.port}`
     });
     //Setup on Events before init connect
     this.client.on('connect', this.onConnect);
