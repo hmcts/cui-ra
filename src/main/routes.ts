4d7b610ce9888228d74899d8c7dc036239da42d1
Ben James <ben.james@hmcts.net>
2023-09-11 17:53:11 +0100

    added history middleware to all routes that should be added to history, cleaned up back route

--- src/main/routes.ts
+++ src/main/routes.ts
@@ -1,7 +1,7 @@
 import * as os from 'os';
 
 import { Route } from './constants';
-import { InitSession, RequireIdam, SchemaValidator, ServiceAuthentication } from './middlewares';
+import { InitSession, History, RequireIdam, SchemaValidator, ServiceAuthentication } from './middlewares';
 import { InboundPayloadSchema } from './schemas';
 
 import { infoRequestHandler } from '@hmcts/info-provider';
@@ -9,27 +9,30 @@ import { Application } from 'express';
 
 const validator = new SchemaValidator();
 const initSession = new InitSession();
+const history = new History();
 
 export default function (app: Application): void {
   // Home Controller
-  app.get(Route.ROOT, app.locals.container.cradle.homeController.get);
-  app.get(Route.OVERVIEW, app.locals.container.cradle.homeController.overview);
-  app.get(Route.INTRO, app.locals.container.cradle.homeController.intro);
+  app.get(Route.ROOT, history.add, app.locals.container.cradle.homeController.get);
+  app.get(Route.OVERVIEW, history.add, app.locals.container.cradle.homeController.overview);
+  app.get(Route.INTRO, history.add, app.locals.container.cradle.homeController.intro);
 
-  app.get(Route.COOKIES, app.locals.container.cradle.homeController.cookies);
-  app.get(Route.PRIVACY_POLICY, app.locals.container.cradle.homeController.privacyPolicy);
-  app.get(Route.TERMS_AND_CONDITIONS, app.locals.container.cradle.homeController.termsAndConditions);
-  app.get(Route.ACCESSIBILITY_STATEMENT, app.locals.container.cradle.homeController.accessibilityStatement);
+  app.get(Route.COOKIES, history.add, app.locals.container.cradle.homeController.cookies);
+  app.get(Route.PRIVACY_POLICY, history.add, app.locals.container.cradle.homeController.privacyPolicy);
+  app.get(Route.TERMS_AND_CONDITIONS, history.add, app.locals.container.cradle.homeController.termsAndConditions);
+  app.get(Route.ACCESSIBILITY_STATEMENT, history.add, app.locals.container.cradle.homeController.accessibilityStatement);
 
   // Demo Controller
   //if (app.locals.ENV !== 'production') {
-  app.get(Route.DEMO, initSession.init, app.locals.container.cradle.demoController.get);
-  app.get(Route.START_DEMO, initSession.init, app.locals.container.cradle.demoController.startDemo);
+  app.get(Route.DEMO, initSession.init, history.add, app.locals.container.cradle.demoController.get);
+  app.get(Route.START_DEMO, initSession.init, history.add, app.locals.container.cradle.demoController.startDemo);
   //}
 
   // Review Controller
-  app.get(Route.REVIEW, initSession.init, app.locals.container.cradle.reviewController.get);
+  app.get(Route.REVIEW, initSession.init, history.add, app.locals.container.cradle.reviewController.get);
+
   app.get(Route.SET_INACTIVE, initSession.init, app.locals.container.cradle.reviewController.setInactive);
+
   app.get(Route.SET_REQUESTED, initSession.init, app.locals.container.cradle.reviewController.setRequested);
 
   //DataController
@@ -39,21 +42,17 @@ export default function (app: Application): void {
   app.use(Route.API_ROOT, new ServiceAuthentication(app.locals.container.cradle.serviceAuth).check);
 
   app.get(Route.BACK, (req, res) => {
-    console.log('GO BACK');
-    console.log(req.session.history);
-    // Navigate back in history if possible
     if (req.session.history && req.session.history.length > 1) {
-      req.session.history.pop(); // Remove the current path
-      const previousPath = req.session.history.pop(); // Get the previous path
+      // Remove the current path
+      req.session.history.pop(); 
+      const previousPath = req.session.history.pop(); 
+
       if (previousPath) {
-        res.redirect(previousPath); // Redirect back to the previous path
-        console.log(req.session.history);
-      } else {
-        res.send('No more history to navigate back');
+        res.redirect(previousPath);
       }
-    } else {
-      res.send('No more history to navigate back');
     }
+    
+    res.send('No more history to navigate back');
   });
 
   //Load all api routes
@@ -65,7 +64,7 @@ export default function (app: Application): void {
   app.get(Route.API_GET_PAYLOAD, app.locals.container.cradle.apiController.getPayload);
 
   // Form Controller
-  app.get(Route.JOURNEY_DISPLAY_FLAGS, initSession.init, app.locals.container.cradle.formController.display);
+  app.get(Route.JOURNEY_DISPLAY_FLAGS, initSession.init, history.add, app.locals.container.cradle.formController.display);
   app.post(Route.JOURNEY_DISPLAY_FLAGS, initSession.init, app.locals.container.cradle.formController.post);
 
   app.get(
