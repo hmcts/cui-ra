6378cc5aae17cf4bed39ae1db90b793619d20757
Ben James <ben.james@hmcts.net>
2023-09-18 19:07:25 +0100

    added new property to store user action on the outbound payload

--- src/main/models/inboundPayload.ts
+++ src/main/models/inboundPayload.ts
@@ -64,6 +64,7 @@ export class MainPayloadFlagPath implements PayloadPath {
 }
 
 export class OutboundPayload {
+  action!: string;
   flagsAsSupplied: MainPayloadDetail | undefined;
   replacementFlags: MainPayloadDetail | undefined;
 }
