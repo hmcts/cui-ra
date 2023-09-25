7b30716c3c5b0cbbf00d06fd91786b1d2411a9f4
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-09-25 14:21:35 +0100

    demo only on aat

--- src/main/assets/js/index.ts
+++ src/main/assets/js/index.ts
@@ -4,23 +4,23 @@ import { initAll } from 'govuk-frontend';
 
 initAll();
 
-var elementExists = document.getElementById("custom-accessible-autocomplete");
-if(elementExists){
+const elementExists = document.getElementById('custom-accessible-autocomplete');
+if (elementExists) {
   accessibleAutocomplete.enhanceSelectElement({
     defaultValue: '',
     selectElement: document.querySelector('#custom-accessible-autocomplete'),
     confirmOnBlur: false,
   });
-  let clearCheckboxes = Array.from(document.querySelectorAll('.clear-autocomplete'));
-  clearCheckboxes.forEach((item) => {
-    item.addEventListener('change', (event) => {
-      const target = event.target as HTMLInputElement
+  const clearCheckboxes = Array.from(document.querySelectorAll('.clear-autocomplete'));
+  clearCheckboxes.forEach(item => {
+    item.addEventListener('change', event => {
+      const target = event.target as HTMLInputElement;
       if (target && target.checked) {
-        var input = document.getElementById("custom-accessible-autocomplete");
-        if(input){
+        const input = document.getElementById('custom-accessible-autocomplete');
+        if (input) {
           (input as HTMLInputElement).value = '';
         }
       }
-    })
+    });
   });
 }
