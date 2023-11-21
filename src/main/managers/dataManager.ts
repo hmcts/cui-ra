2cfc7e60989fa2eaad785a30ff43589e89058575
Sonny Lloyd <sonny.lloyd@hmcts.net>
2023-11-21 14:49:19 +0000

    fixed payload and time to iso

--- src/main/managers/dataManager.ts
+++ src/main/managers/dataManager.ts
@@ -80,11 +80,6 @@ export class DataManager<T> implements DataManagerInterface<T> {
 
   public getDateTime(): string {
     const now: Date = new Date();
-    return `${now.getDate().toString().padStart(2, '0')}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now
-      .getFullYear()
-      .toString()} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now
-      .getSeconds()
-      .toString()
-      .padStart(2, '0')}`;
+    return now.toISOString().replace('Z', '');
   }
 }
