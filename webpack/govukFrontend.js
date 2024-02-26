673ecd2b1e3275040b53af4c5dc093d65e2f4ed1
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-02-26 13:05:17 +0000

    Address govuk change

--- webpack/govukFrontend.js
+++ webpack/govukFrontend.js
@@ -7,6 +7,7 @@ const root = path.resolve(rootExport, '..');
 const sass = path.resolve(root, 'all.scss');
 const javascript = path.resolve(root, 'all.js');
 const components = path.resolve(root, 'components');
+const macros = path.resolve(root, 'macros');
 const assets = path.resolve(root, 'assets');
 const images = path.resolve(assets, 'images');
 const fonts = path.resolve(assets, 'fonts');
@@ -17,11 +18,12 @@ const copyGovukTemplateAssets = new CopyWebpackPlugin({
     { from: fonts, to: 'assets/fonts' },
     { from: `${root}/template.njk`, to: '../views/govuk' },
     { from: `${root}/components`, to: '../views/govuk/components' },
+    { from: `${root}/macros`, to: '../views/govuk/macros' },
     { from: `${assets}/manifest.json`, to: 'assets/manifest.json' },
   ],
 });
 
 module.exports = {
-  paths: { template: root, components, sass, javascript, assets },
+  paths: { template: root, components, macros, sass, javascript, assets },
   plugins: [copyGovukTemplateAssets],
 };
