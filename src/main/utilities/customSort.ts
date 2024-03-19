808925a1acb65b4d71fbb6b7f96144578da09e06
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-03-19 17:55:41 +0000

    CUIRA-296 update ordering

--- src/main/utilities/customSort.ts
+++ src/main/utilities/customSort.ts
@@ -6,6 +6,23 @@ export class CustomSort {
   public static alphabeticalAsc<T>(items: T[], req: Request): T[] {
     return items.sort((a, b) => {
       //Add short if to check if welsh and use name_cy
+      const name = req.session.welsh ? 'name_cy' : 'name';
+      const nameA = a['value'][name].toUpperCase(); // Convert to uppercase for case-insensitive sorting
+      const nameB = b['value'][name].toUpperCase();
+
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+
+  public static alphabeticalAscOtherLast<T>(items: T[], req: Request): T[] {
+    return items.sort((a, b) => {
+      //Add short if to check if welsh and use name_cy
       const nameA = req.session.welsh ? a['value'].name_cy.toUpperCase() : a['value'].name.toUpperCase(); // Convert to uppercase for case-insensitive sorting
       const flagCodeA = a['value'].flagCode;
       const nameB = req.session.welsh ? b['value'].name_cy.toUpperCase() : b['value'].name.toUpperCase();
--- src/test/unit/tests/utilities/customSort.test.ts
+++ src/test/unit/tests/utilities/customSort.test.ts
@@ -4,89 +4,51 @@ import { Request } from 'express';
 
 describe('CustomSort', () => {
   describe('sortAlphabetically', () => {
-    const items: PayloadCollectionItem[] = [
-      {
-        id: 'first item',
-        value: {
-          name: 'cat',
-          name_cy: 'cat',
-          subTypeValue: '',
-          subTypeValue_cy: '',
-          flagComment: '',
-          flagComment_cy: '',
-          flagUpdateComment: '',
-          otherDescription: '',
-          otherDescription_cy: '',
-          subTypeKey: '',
-          dateTimeModified: '12-07-2023 13:28:21',
-          dateTimeCreated: '12-07-2023 13:28:21',
-          path: [
-            {
-              id: '',
-              name: '',
-            },
-          ],
-          hearingRelevant: 'No',
-          flagCode: 'CF0001',
-          status: 'Active',
-          availableExternally: 'No',
-        },
+    const item1 = {
+      id: 'first item',
+      value: {
+        name: 'cat',
+        name_cy: 'cat_cy',
+        flagCode: 'CF0001',
       },
-      {
-        id: 'second item',
-        value: {
-          name: 'zebra',
-          name_cy: 'zebra',
-          subTypeValue: '',
-          subTypeValue_cy: '',
-          flagComment: '',
-          flagComment_cy: '',
-          flagUpdateComment: '',
-          otherDescription: '',
-          otherDescription_cy: '',
-          subTypeKey: '',
-          dateTimeModified: '12-07-2023 13:28:21',
-          dateTimeCreated: '12-07-2023 13:28:21',
-          path: [
-            {
-              id: '',
-              name: '',
-            },
-          ],
-          hearingRelevant: 'No',
-          flagCode: 'CF0001',
-          status: 'Active',
-          availableExternally: 'No',
-        },
+    } as PayloadCollectionItem;
+    const item2 = {
+      id: 'second item',
+      value: {
+        name: 'zebra',
+        name_cy: 'zebra_cy',
+        flagCode: 'CF0001',
       },
-      {
-        id: 'third item',
-        value: {
-          name: 'apple',
-          name_cy: 'apple',
-          subTypeValue: '',
-          subTypeValue_cy: '',
-          flagComment: '',
-          flagComment_cy: '',
-          flagUpdateComment: '',
-          otherDescription: '',
-          otherDescription_cy: '',
-          subTypeKey: '',
-          dateTimeModified: '12-07-2023 13:28:21',
-          dateTimeCreated: '12-07-2023 13:28:21',
-          path: [
-            {
-              id: '',
-              name: '',
-            },
-          ],
-          hearingRelevant: 'No',
-          flagCode: 'CF0001',
-          status: 'Active',
-          availableExternally: 'No',
-        },
+    } as PayloadCollectionItem;
+
+    const item3 = {
+      id: 'third item',
+      value: {
+        name: 'apple',
+        name_cy: 'apple_cy',
+        flagCode: 'CF0001',
       },
-    ];
+    } as PayloadCollectionItem;
+
+    const item4 = {
+      id: 'fourth item',
+      value: {
+        name: 'other',
+        name_cy: 'other_cy',
+        flagCode: 'OT0001',
+      },
+    } as PayloadCollectionItem;
+
+    const item5 = {
+      id: 'fifth item',
+      value: {
+        name: 'banana',
+        name_cy: 'banana_cy',
+        flagCode: 'CF0001',
+      },
+    } as PayloadCollectionItem;
+
+    const items: PayloadCollectionItem[] = [item1, item2, item3, item4, item5];
 
     const mockRequest = (_welsh: boolean = false): Request => {
       return {
@@ -96,20 +58,44 @@ describe('CustomSort', () => {
       } as unknown as Request;
     };
 
-    test('Should sort the data provided by name (english)', () => {
-      var sortedItems = CustomSort.alphabeticalAsc<PayloadCollectionItem>(items, mockRequest(false));
+    test('Should sort the data provided by name (english) putting other as last', () => {
+      var sortedItems = CustomSort.alphabeticalAscOtherLast<PayloadCollectionItem>(items, mockRequest(false));
 
       expect(sortedItems[0].value.name).toBe('apple');
-      expect(sortedItems[1].value.name).toBe('cat');
-      expect(sortedItems[2].value.name).toBe('zebra');
+      expect(sortedItems[1].value.name).toBe('banana');
+      expect(sortedItems[2].value.name).toBe('cat');
+      expect(sortedItems[3].value.name).toBe('zebra');
+      expect(sortedItems[4].value.name).toBe('other');
+    });
+
+    test('Should sort the data provided by name_cy (welsh) putting other as last', () => {
+      var sortedItems = CustomSort.alphabeticalAscOtherLast<PayloadCollectionItem>(items, mockRequest(true));
+
+      expect(sortedItems[0].value.name_cy).toBe('apple_cy');
+      expect(sortedItems[1].value.name_cy).toBe('banana_cy');
+      expect(sortedItems[2].value.name_cy).toBe('cat_cy');
+      expect(sortedItems[3].value.name_cy).toBe('zebra_cy');
+      expect(sortedItems[4].value.name_cy).toBe('other_cy');
+    });
+
+    test('Should sort the data provided by name (english)', () => {
+      var sortedItems = CustomSort.alphabeticalAsc<PayloadCollectionItem>(items, mockRequest(true));
+
+      expect(sortedItems[0].value.name_cy).toBe('apple_cy');
+      expect(sortedItems[1].value.name_cy).toBe('banana_cy');
+      expect(sortedItems[2].value.name_cy).toBe('cat_cy');
+      expect(sortedItems[3].value.name_cy).toBe('other_cy');
+      expect(sortedItems[4].value.name_cy).toBe('zebra_cy');
     });
 
     test('Should sort the data provided by name_cy (welsh)', () => {
       var sortedItems = CustomSort.alphabeticalAsc<PayloadCollectionItem>(items, mockRequest(true));
 
-      expect(sortedItems[0].value.name).toBe('apple');
-      expect(sortedItems[1].value.name).toBe('cat');
-      expect(sortedItems[2].value.name).toBe('zebra');
+      expect(sortedItems[0].value.name_cy).toBe('apple_cy');
+      expect(sortedItems[1].value.name_cy).toBe('banana_cy');
+      expect(sortedItems[2].value.name_cy).toBe('cat_cy');
+      expect(sortedItems[3].value.name_cy).toBe('other_cy');
+      expect(sortedItems[4].value.name_cy).toBe('zebra_cy');
     });
   });
 });
