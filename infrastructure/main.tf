53ae9c874c73673ad04d1cafc9f89047aeb78187
Rapolas Kaselis <rapolas.kaselis@hmcts.net>
2024-08-27 13:57:40 +0100

    add location for app insights

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -38,6 +38,7 @@ module "application_insights" {
   env     = var.env
   product = var.product
   name    = var.product
+  location = "UK South"
 
   resource_group_name = azurerm_resource_group.rg.name
 
