82c9a4c3f9de9470ada9909695804895082b5033
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-08-28 11:28:47 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,9 +35,9 @@ resource "azurerm_key_vault_secret" "app-insights-connection-string" {
 module "application_insights" {
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
-  env      = var.env
-  product  = var.product
-  name     = var.product
+  env     = var.env
+  product = var.product
+  name    = var.product
 
   resource_group_name = azurerm_resource_group.rg.name
 
